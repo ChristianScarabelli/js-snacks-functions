f75d266d11c547c2d7fe0802b4c0c5e93d7885e9
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-21 17:00:16 +0200

    ADD: 03 Conta vocali arrow functions

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -30,6 +30,40 @@ function howManyVocals(string) {
     return vocals;  // Restituiamo il numero di vocali
 }
 
+// Arrow function risultato esplicito 
+const howManyVocals = (string) => {
+    string = string.toLowerCase();  // Uniformiamo la stringa in minuscolo
+
+    let vocals = 0  // Variabile per contare le vocali impostata a 0, per poi incrementarsi
+
+    // Ciclo per attraversare ogni carattere della stringa
+    for (let i = 0; i < string.length; i++) {
+        let char = string.charAt(i);  // Prendiamo il carattere alla posizione i
+
+        // Se il carattere è una vocale, incrementiamo il conteggio
+        if (
+            char === "a" ||
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u"
+        ) {
+            vocals++;
+        }
+    }
+    return vocals;  // Restituiamo il numero di vocali
+}
+
+
+// Arrow function risultato implicito  
+const howManyVocals = (string) =>
+    string
+.toLowerCase()   // Convertiamo la stringa in minuscolo
+.split('')       // Dividiamo la stringa in un array di caratteri
+.filter(char => "aeiou".includes(char))  // Filtriamo solo le vocali
+.length         // Contiamo quante vocali ci sono
+
+
 // Invoca la funzione e stampa il risultato in console
 const numberOfVocals = howManyVocals(word);
 console.log('Numero di vocali:', numberOfVocals);  // Risultato atteso: 3 (a, a, i)
