4ec39fa00540e7183ca03036e7124b9c1d1cf2c3
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-21 17:15:58 +0200

    ADD: 04 Saluti solved

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = 'Mario'
 
 
 // Dichiara la funzione qui.
+// Funzione tradizionale 
+function sayHiTo (string) {
+    return `Ciao ${name}`
+}
 
+// Arrow function ritorno implicito
+const sayHiTo = (name) => `Ciao ${name}`
+console.log(sayHiTo(name))  //Risultato atteso se si passa 'Mario': // ciao Mario
 
 // Invoca la funzione qui e stampa il risultato in console
+const greetings = sayHiTo(name)
+console.log(greetings)  //Risultato atteso se si passa 'Mario': // ciao Mario
 
-
-
-//Risultato atteso se si passa 'Mario': // ciao Mario
